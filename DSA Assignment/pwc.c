d52dd655962bde54529b67be8f54d86eeea668d4
dev-aakash1213 <90676815+dev-aakash1213@users.noreply.github.com>
2022-09-24 18:34:54 +0530

    C DSA

---
+++ DSA Assignment/pwc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+   char a,b[50],c[100];
+   
+    scanf("%c", &a);
+    scanf("%s", &b);
+    scanf(" %[^\n]%*c", &c);
+    
+    printf("%c\n%s\n%s",a,b,c);
+    
+    return 0;
+}
