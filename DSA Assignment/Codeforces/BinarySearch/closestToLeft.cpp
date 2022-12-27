30f7d4d3b1ee687082be01a23283991ef3f2ebee
Aakash Amod Rajput <90676815+dev-aakash1213@users.noreply.github.com>
2022-12-27 15:35:16 +0530

    Done with Binary Searching.

---
+++ DSA Assignment/Codeforces/BinarySearch/closestToLeft.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int closeToRight(vector<int> A,int n, int target)
+{
+    int l = -1, r = n; 
+    while (r - l > 1) {
+        int mid = l + (r - l) / 2;
+
+        if (target <= A[mid])
+            r = mid;
+        else
+            l = mid;
+    }
+
+    return r;
+}
+
+int main()
+{
+    int n, k;
+    cin>>n>>k;
+
+    vector<int> input(n);
+    for (int i = 0; i < n; i++)
+       cin>>input[i];
+
+    for (int i = 0; i < k; i++) {
+        int num;
+        cin>>num;
+
+        cout<<closeToRight(input, n, num )+ 1<<endl;
+    }
+
+    return 0;
+}
